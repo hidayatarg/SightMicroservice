ea284f23a7c047f5390e4a481f540e1b3160b7f0
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-11-14 21:43:58 +0300

    Catalog API - Dto Classes Implemented

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CourseDto
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Picture { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public FeatureDto Feature { get; set; }
+
+        // For relationship with Category
+        public string CategoryId { get; set; }
+
+        // Only for code side
+        public CategoryDto Category { get; set; }
+    }
+}
