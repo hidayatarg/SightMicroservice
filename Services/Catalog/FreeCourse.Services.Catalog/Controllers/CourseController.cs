5e8320e3d561e140f008487a53322436d9e34dae
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-11-17 02:46:40 +0300

    Catalog API - CourseController Implemented GetById and GetAll

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -0,0 +1,40 @@
+﻿using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CourseController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+
+        internal CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+            return CreateActionResultInstance(response);
+        }
+
+        public async Task<IActionResult> GetAllBy(string id)
+        {
+            var response = await _courseService.GetAllAsync();
+            return CreateActionResultInstance(response);
+        }
+
+
+
+
+    }
+}
