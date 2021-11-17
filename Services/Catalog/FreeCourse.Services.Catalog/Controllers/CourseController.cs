a02da8735dca807237c7d72c546bec4dfeb372e3
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-11-17 19:17:03 +0300

    Catalog API - CourseControler Completed

--- Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreeCourse.Services.Catalog.Dtos;
 
 namespace FreeCourse.Services.Catalog.Controllers
 {
@@ -33,8 +34,32 @@ namespace FreeCourse.Services.Catalog.Controllers
             return CreateActionResultInstance(response);
         }
 
-
-
-
+        [Route("api/[controller]/GetByUserId/{userId}")]
+        public async Task<IActionResult> GetByUserId(string userId)
+        {
+            var response = await _courseService.GetAllByUserIdAync(userId);
+            return CreateActionResultInstance(response);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateDto courseCreateDto)
+        {
+            var response = await _courseService.CreateAsync(courseCreateDto);
+            return CreateActionResultInstance(response);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Update(CourseUpdateDto courseUpdateDto)
+        {
+            var response = await _courseService.UpdateAsync(courseUpdateDto);
+            return CreateActionResultInstance(response);
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _courseService.DeleteAsync(id);
+            return CreateActionResultInstance(response);
+        }
     }
 }
