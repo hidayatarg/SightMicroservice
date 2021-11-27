ded0747d22883f3b5447eda8ca2fcc030cdbc345
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-11-27 03:58:29 +0300

    IdentityServer - Implemented with Migrations

---
+++ IdentityServer/FreeCourse.IdentityServer/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FreeCourse.IdentityServer.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        // Inherit properties from IdentityUser
+        // Extra Fields
+
+        public string City { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+#### Portainer Image
+`docker run -d -p 8000:9000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce`
